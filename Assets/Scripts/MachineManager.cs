c15bbfc9f905fb62be8af2c64182f9e768035e41
a24caisu <a24caisu@student.his.se>
2026-02-28 15:37:13 +0100

    Påbörjat machine script

--- Assets/Scripts/MachineManager.cs
+++ Assets/Scripts/MachineManager.cs
@@ -1,16 +1,32 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MachineManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private string acceptedPant;
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(acceptedPant))
+        {
+         
+        }
+        else
+        {
+            
+        }
+        throw new NotImplementedException();
     }
 }
