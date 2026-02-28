67297ecdca235ed6b7348a8cd339b4f5186b04fc
a24caisu <a24caisu@student.his.se>
2026-02-28 22:26:43 +0100

    Ändrat med Upgrades

--- Assets/Scripts/MachineUpgrades.cs
+++ Assets/Scripts/MachineUpgrades.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class MachineUpgrades : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class MachineUpgrades : MonoBehaviour
     {
       upgrade = new UpgradeEnum();
       upgrade = UpgradeEnum.First;
+      gameObject.GetComponent<Image>().color =  new Color(150, 201, 88);
     }
     
     public void NewMachine(int cost)
@@ -27,22 +29,27 @@ public class MachineUpgrades : MonoBehaviour
             score.Score -= cost;
             rullband.Pant.Add(storPET);
             upgrade = UpgradeEnum.Second;
+            glassMachine.gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+            Debug.Log("upgrade");
         }
 
-        if (upgrade == UpgradeEnum.Second && score.Score >= cost)
+        else if (upgrade == UpgradeEnum.Second && score.Score >= cost * 2)
         {
+            Debug.Log("Upgrade 2");
             score.Score -= cost;
-            metalMachine.SetActive(true);
+            metalMachine.gameObject.GetComponent<CircleCollider2D>().enabled = true;
             metalMachine.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+            upgrade = UpgradeEnum.Third;
             rullband.Pant.Add(metalCan);
         }
 
-        if (upgrade == UpgradeEnum.Third && score.Score >= cost)
-        {
+       else  if (upgrade == UpgradeEnum.Third && score.Score >= cost * 3)
+        {Debug.Log("Upgrade 3");
             score.Score -= cost;
-            glassMachine.SetActive(true);
+            glassMachine.gameObject.GetComponent<CircleCollider2D>().enabled = true;
             glassMachine.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
             rullband.Pant.Add(glassBottle);
+            gameObject.GetComponent<Image>().color = Color.gray;
         }
     }
 }
