003b42c74e312d61e6de59b07c4c81a0cf21a9e0
Treemaster134 <agh.lamberth@gmail.com>
2026-02-28 17:00:05 +0100

    Started work on throwing

--- Assets/Scripts/ThrowController.cs
+++ Assets/Scripts/ThrowController.cs
@@ -5,8 +5,11 @@ public class ThrowController : MonoBehaviour
 {
     
     private InputAction aimAction;
+    private InputAction throwAction;
     private LineRenderer lineRenderer;
+    private Rigidbody2D rb;
     private Camera cam;
+    private bool holdingThrow = false;
     [SerializeField] private float maxDistance;
     public PantInformation info;
     
@@ -14,35 +17,35 @@ public class ThrowController : MonoBehaviour
     void Start()
     {
         aimAction = InputSystem.actions.FindAction("Aim");
+        throwAction = InputSystem.actions.FindAction("Throw");
+        
         lineRenderer = GetComponent<LineRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(aimAction?.activeControl?.displayName != null)
+        if (holdingThrow)
         {
-            Vector2 mousePosition = aimAction.ReadValue<Vector2>();
-            Vector2 target = transform.position - cam.ScreenToWorldPoint(new Vector3(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y), 10));
-            target = ClampMagnitude(target);
-            float strength = target.magnitude;
+            Vector3 delta = Vector3.zero;
+            if(throwAction.IsPressed())
+            {
+                Vector2 mousePosition = aimAction.ReadValue<Vector2>();
+                delta = cam.ScreenToWorldPoint(new Vector3(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y), 0)) - transform.position;
+                float strength = Mathf.Min(maxDistance, delta.magnitude);
             
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, target);
-        }
-    }
-
-    Vector2 ClampMagnitude(Vector2 v)
-    {
-        float currentMagnitude = v.magnitude;
-
-        if (currentMagnitude > maxDistance)
-        {
-            float multiplyBy = maxDistance/currentMagnitude;
-            return new Vector2(v.x * multiplyBy, v.y * multiplyBy);
+                lineRenderer.SetPosition(0, transform.position);
+                lineRenderer.SetPosition(1, transform.position - delta.normalized * strength);
+            }
+            else
+            {
+                rb.simulated = true;
+                rb.linearVelocity = delta;
+            }
         }
 
-        return v;
+        holdingThrow = throwAction.IsPressed();
     }
 }
