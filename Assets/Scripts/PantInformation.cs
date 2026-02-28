8ecb5c35cf2212bad54a747756ca049325833de0
Treemaster134 <agh.lamberth@gmail.com>
2026-02-28 23:07:58 +0100

    delete out of bounds

--- Assets/Scripts/PantInformation.cs
+++ Assets/Scripts/PantInformation.cs
@@ -4,4 +4,12 @@
 public class PantInformation : MonoBehaviour
 {
     public int value;
+
+    void Update()
+    {
+        if (transform.position.x < -10.0f || transform.position.x > 10.0f || transform.position.y < -7.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
