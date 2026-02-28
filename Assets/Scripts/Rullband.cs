57d95f1b4062ba1b95efef849f923819ef154f5e
a24caisu <a24caisu@student.his.se>
2026-02-28 16:59:07 +0100

    Rullbandscript

--- Assets/Scripts/Rullband.cs
+++ Assets/Scripts/Rullband.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 public class Rullband : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private BoxCollider2D one;
-    [SerializeField] private BoxCollider2D two;
-    [SerializeField] private BoxCollider2D three;
+    [SerializeField] private Transform one;
+    [SerializeField] private Transform two;
+    [SerializeField] private Transform three;
+    [SerializeField]private GameObject petfalska;
     void Start()
     {
       
@@ -15,7 +16,11 @@ public class Rullband : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (!Physics2D.OverlapBox(three.position, Vector3.one * 0.1f, 0))
+        {
+            Instantiate(petfalska, three.position, three.rotation);
+        }
+        
     }
 
     
