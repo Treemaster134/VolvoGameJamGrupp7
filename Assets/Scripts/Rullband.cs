4a5d94f56acc5f619f2cdb45ffe17d7dbfc8041d
a24caisu <a24caisu@student.his.se>
2026-02-28 16:42:27 +0100

    RullbandsPrefab

---
+++ Assets/Scripts/Rullband.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Rullband : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private BoxCollider2D one;
+    [SerializeField] private BoxCollider2D two;
+    [SerializeField] private BoxCollider2D three;
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    
+}
